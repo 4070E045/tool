09c1ac2f408a52063e5ac996430e6c6fc4ee7bf7
JIm <43465687+4070E045@users.noreply.github.com>
2019-10-19 14:09:29 +0800

    Add files via upload

--- sniffer_with_icmp.py
+++ sniffer_with_icmp.py
@@ -49,20 +49,39 @@ class IP(Structure):
     if os.name == "nt":
       sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
 class ICMP(Structure):   
- _fields_=[
-  ("type",                c_ubyte),
-  ("code",                c_ubyte),
-  ("checksum",            c_ushort),
-  ("unused",              c_ushort),
-  ("next_hop_mtu",        c_ushort),
-  ]
-def_new_(self, socket_buffer):
-  return self.from_buffer_copy(socket_buffer)
-def_init_(self, socket_buffer=None):
-  self, protocol_map = {1:"ICMP", 6:"TCP", 17:"UDP"}
-  self.src_address = socket.inet_ntoa(strust.pack("<L",self.src))
-  self.dst_address = socket.inet_ntoa(strust.pack("<L",self.dst))
-  try:
-    self, protocol = self.protocol_map[self.protocol_num]
-  except:
-    self, protocol = str(self.protocol_num)
+    _fields_=[
+        ("type",                c_ubyte),
+        ("code",                c_ubyte),
+        ("checksum",            c_ushort),
+        ("unused",              c_ushort),
+        ("next_hop_mtu",        c_ushort),
+    ]
+    def_new_(self, socket_buffer):
+        return self.from_buffer_copy(socket_buffer)
+    def_init_(self, socket_buffer=None):
+        self, protocol_map = {1:"ICMP", 6:"TCP", 17:"UDP"}
+        self.src_address = socket.inet_ntoa(strust.pack("<L",self.src))
+        self.dst_address = socket.inet_ntoa(strust.pack("<L",self.dst))
+    try:
+        self, protocol = self.protocol_map[self.protocol_num]
+    except:
+        self, protocol = str(self.protocol_num)
+    if os.name == "nt":
+    socket_protocol = socket.IPPROTO_IP
+    else:
+    socket_protocol = socket.IPPROTO_ICMP
+    sniffer = socket.socket(socket.AF_INET, socketSOCK_RAW, socket_protocol)
+    sniffer.bind((host,0))
+    sinffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    if os.name =="nt":
+    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    try:
+        while True:
+        raw_buffer = sniffer sniffer.recvfrom(65565)[0]
+        ip_header = IP(raw_buffer[0:20])
+        print "protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
+        if ip_header.protocol == "ICMP":
+            offset = ip_header.ih1 *4
+            buf = raw_buffer[offset:offset + sizeof(ICMP)]
+            icmp_buffer = ICMP(buf)
+            print "ICMP -> Type: %d Code: %d" % (icmp_header.type,icmp_header.code)
